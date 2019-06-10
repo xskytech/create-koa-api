f3115aa300e8865a904417b3ddbe5c001c7bffbf
narekkeryan <narek.keryan@gmail.com>
2019-06-11 01:29:43 +0400

    Add functionality to clone branch for specific version

--- bin/index.js
+++ bin/index.js
@@ -7,6 +7,8 @@ const logger = require('log4js').getLogger('./bin/index.js');
 
 const { generatePackageJson } = require('./lib');
 
+const { version: packageVersion } = require('../package.json');
+
 const projectDirectory = process.argv[2] || 'koa-api';
 
 const remoteOrigin = 'git@github.com:xskytech/koa-api.git';
@@ -16,7 +18,7 @@ logger.level = 'debug';
 logger.info('Initializing project...');
 
 exec(
-  `git clone ${remoteOrigin} ${projectDirectory} && cd ${projectDirectory} && rm -rf .git && git init`,
+  `git clone --single-branch --branch ${packageVersion} ${remoteOrigin} ${projectDirectory} && cd ${projectDirectory} && rm -rf .git && git init`,
   async (error) => {
     if (error) {
       logger.error(error.message || error);
